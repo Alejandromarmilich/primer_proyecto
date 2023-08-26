81b2ddd21df437d23c1d05ad8070c623419823f2
Alejandro <33562302+Alejandromarmilich@users.noreply.github.com>
2023-08-26 00:52:54 -0300

    New footer

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -1,7 +1,61 @@
-    <!--Inicio Footer-->
-    <footer class="footer-content">
-        <div class='container my-3'>
-            <div class='row row-cols-4 justify-content-center'>
+    <!--Inicio Nuevo Footer-->
+    <footer class="container-fluid">
+      <div class="row p-5 bg-dark text-white"> 
+
+        <div class="col-xs-12 col-nd-12 col-lg-3">
+          
+          <div class="mb-3">
+          <img src="./assets/img/glow_logo.webp" alt="logo marca" width='90'>
+          </div>
+          <div class="mb-3 d-flex justify-content-center">
+            <p class="h6">Glow Web Design. | &copy; Copyright 2023</p>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-nd-12 col-lg-3">
+          <p class="h5 d-flex justify-content-center mt-3">Menú</p>
+          <div class="mb-1 d-flex justify-content-center">
+            <a class="text-white text-decoration-none" href="principal"
+              >Inicio</a
+            >
+          </div>
+          <div class="mb-1 d-flex justify-content-center">
+            <a class="text-white text-decoration-none" href="catalogo"
+              >Catálogo</a
+            >
+          </div>
+          <div class="mb-1 d-flex justify-content-center">
+            <a class="text-white text-decoration-none" href="quienes_somos"
+              >Quienes Somos</a
+            >
+          </div>
+          <div class="mb-1 d-flex justify-content-center">
+            <a class="text-white text-decoration-none" href="acerca_de"
+              >Acerca de</a
+            >
+          </div>
+        </div>
+        <div class="col-xs-12 col-nd-12 col-lg-3">
+          <p class="h5 d-flex justify-content-center mt-3">Usuarios</p>
+          <div class="mb-1 d-flex justify-content-center">
+            <a
+              class="text-white text-decoration-none"
+              href=""
+              >Iniciar sesión</a
+            >
+          </div>
+          <div class="mb-1 d-flex justify-content-center">
+            <a
+              class="text-white text-decoration-none"
+              href="registro"
+              >Registrarse</a
+            >
+          </div>
+        </div>
+        
+        <div class="col-xs-12 col-nd-12 col-lg-3">
+            <p class="h5 mb-3 d-flex justify-content-center mt-3">Contacto</p>
+            <div class="row justify-content-around">
                 <!-- Facebook -->
                 <div class='col-auto contenedor-iconos'>
                     <a href="https://www.facebook.com/" target='_blank' rel='norrefer norreper'><img src="./assets/icons/facebook.svg" alt="Facebook"></a>
@@ -17,16 +71,12 @@
                 <!-- Whatsapp -->
                 <div class='col-auto contenedor-iconos'>
                     <a href="https://wa.me/549113334444" target='_blank' rel='norrefer norreper'><img src="./assets/icons/whatsapp.svg" alt="Whatsapp"></a>                    
-                </div>                
+                </div> 
             </div>
+             
         </div>
-        <div class='container my-3 text-center'>
-            <p>Derechos de autor &copy; 2023 Glow Web Design. <br> Todos los derechos reservados</p>
-        </div>        
+      </div>
     </footer>
-    <!--Fin Footer-->
-    <!--Inicio Nuevo Footer-->
-    
     <!--Fin Nuevo Footer-->
     <script src=".\assets\js\bootstrap.bundle.min.js"></script>
 </body>
