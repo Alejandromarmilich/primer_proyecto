9bdefdd9564cb5ab7c26fdb7307a941a5bbea625
Guillermo García <guijujo.25@gmail.com>
2023-09-22 18:20:17 -0300

    Se corrigio el registrarse aqui estaba en color blanco

--- app/Views/back/usuario/login.php
+++ app/Views/back/usuario/login.php
@@ -1,7 +1,7 @@
 <!-- Inicio seccion login -->
 
 <section id="login" class="container my-5 px-5 d-flex justify-content-center">
-    <div class="col-12 col-md-8 col-lg-4 bg-body my-5 pb-5 rounded">
+    <div class="col-12 col-md-6 col-lg-3 bg-body my-5 pb-5 rounded">
         <div class="my-5 text-center">
             <h2 class="text-black">Iniciar sesion</h2>
         </div>
@@ -9,9 +9,9 @@
         <!-- Mensaje de error -->
         <div class="text-black">
             <?php if (session()->getFlashdata('msg')) : ?>
-                <div class="alert alert-warning" role="alert">
-                    <?php session()->getFlashData('msg') ?>
-                </div>
+            <div class="alert alert-warning" role="alert">
+                <?php session()->getFlashData('msg') ?>
+            </div>
             <?php endif; ?>
         </div>
 
@@ -21,20 +21,25 @@
             <div class="card-body" media="(max-width:768px)">
                 <div class="mb-2">
                     <div class="form-floating my-3">
-                        <input name="email" type="text" class="form-control" id="floatingInput" placeholder="nombre@ejemplo.com">
+                        <input name="email" type="text" class="form-control" id="floatingInput"
+                            placeholder="nombre@ejemplo.com">
                         <label for="floatingInput">Correo electrónico</label>
                     </div>
                 </div>
 
                 <div class="form-floating">
-                    <input name="pass" type="password" class="form-control" id="floatingPassword" placeholder="Contraseña">
+                    <input name="pass" type="password" class="form-control" id="floatingPassword"
+                        placeholder="Contraseña">
                     <label for="floatingPassword">Contraseña</label>
                 </div>
                 <div class="container mt-3">
                     <input type="submit" value="Ingresar" class="btn btn-primary">
 
                     <a href="<?php echo base_url('login'); ?>" class="btn btn-danger">Cancelar</a>
-                    <br><span class="mt-3">¿Aún no se registró? <a href="<?php echo base_url('/registro') ?>">Registrarse aquí</a></span>
+                </div>
+                <div class="mt-3">
+                    <span class="mt-3">¿Aún no se registró?</span>
+                    <a href="registro" class="text-black">Registrarse aquí</a>
                 </div>
             </div>
         </form>
