0db453f327b93ae477d9bc7fc8e69c5d2021934a
Guillermo García <guijujo.25@gmail.com>
2023-09-24 16:09:53 -0300

    !

--- app/Views/back/usuario/usuario_logueado.php
+++ app/Views/back/usuario/usuario_logueado.php
@@ -7,22 +7,24 @@
                 <br><br>
                 <div class="container my-5 col-6 d-flex justify-content-center">
                     <?php if (session()->getFlashdata('msg')) : ?>
-                        <div class="alert alert-warning">
-                            <?= session()->getFlashdata('msg') ?>
-                        </div>
+                    <div class="alert alert-warning">
+                        <?= session()->getFlashdata('msg') ?>
+                    </div>
                     <?php endif; ?>
                 </div>
                 <?php if (session()->perfil_id == 1) : ?>
-                    <div class="container">
-                        <img class="d-flex justify-content-center" height="300px" width="300px" src="<?php echo base_url('/assets/img/admin.png'); ?>" alt="admin">
-                    </div>
+                <div class="container">
+                    <img class="d-flex justify-content-center" height="300px" width="300px"
+                        src="<?php echo base_url('/assets/img/admin.png'); ?>" alt="admin">
+                </div>
                 <?php elseif (session()->perfil_id == 2) : ?>
-                    <div class="container">
-                        <img class="d-flex justify-content-center my-5" height="200px" width="200px" src="<?php echo base_url('/assets/img/cliente.png'); ?>" alt="cliente">
-                    </div>
+                <div class="container">
+                    <img class="d-flex justify-content-center my-5" height="200px" width="200px"
+                        src="<?php echo base_url('/assets/img/cliente.png'); ?>" alt="cliente">
+                </div>
             </div>
 
-        <?php endif; ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
