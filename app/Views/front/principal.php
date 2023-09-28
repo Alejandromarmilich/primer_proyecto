80008b922e95d09f004d6ca18ce0931f230e3da1
Guillermo García <guijujo.25@gmail.com>
2023-09-28 16:52:17 -0300

    principal

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,68 +1,81 @@
 <!-- Seccion principal -->
 <section id="principal" class="container my-5 mx-md-auto p-md-5">
-  <div class="container my-5 my-md-4 bd-layout">
+    <div class="container my-5 my-md-4 bd-layout">
 
-    <h2 class='text-center'>Glow Technology</h2>
-    <p class='parrafo'>
-      Descubre un mundo de posibilidades en la palma de tu mano. Los celulares no son solo dispositivos, son ventanas hacia un universo de conectividad, información y entretenimiento. Desde capturar momentos inolvidables con cámaras de alta definición hasta explorar aplicaciones que hacen tu vida más fácil, los celulares son tu entrada a un futuro tecnológico emocionante.
-    </p>
+        <h2 class='text-center'>Glow Technology</h2>
+        <p class='parrafo'>
+            Descubre un mundo de posibilidades en la palma de tu mano. Los celulares no son solo dispositivos, son
+            ventanas hacia un universo de conectividad, información y entretenimiento. Desde capturar momentos
+            inolvidables con cámaras de alta definición hasta explorar aplicaciones que hacen tu vida más fácil, los
+            celulares son tu entrada a un futuro tecnológico emocionante.
+        </p>
 
-    <p class='parrafo'>
-      Imagina poder explorar ciudades desconocidas a través de mapas interactivos, mantenerte conectado con amigos y familiares independientemente de la distancia, y sumergirte en experiencias visuales envolventes como nunca antes. La velocidad y potencia de los celulares modernos te permiten disfrutar de tus pasiones donde sea que vayas: juegos, música, películas y más.
-    </p>
+        <p class='parrafo'>
+            Imagina poder explorar ciudades desconocidas a través de mapas interactivos, mantenerte conectado con amigos
+            y familiares independientemente de la distancia, y sumergirte en experiencias visuales envolventes como
+            nunca antes. La velocidad y potencia de los celulares modernos te permiten disfrutar de tus pasiones donde
+            sea que vayas: juegos, música, películas y más.
+        </p>
 
-    <!-- Inicio del carrusel -->
-    <div class='contenedor-carousel'>
-      <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        <!-- Inicio del carrusel -->
+        <div class='contenedor-carousel'>
+            <div id="carouselExampleCaptions" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                        aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+                        aria-label="Slide 4"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./assets/img/001.webp" class="d-block w-100" alt="camara noche">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Cámara</h5>
+                            <p>Saca las mejores fotos con su nueva cámara de alta definición, incluso de noche.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/img/002.webp" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Redes sociales</h5>
+                            <p>Mantente conectado a todos lados desde un solo dispositivo.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/img/003.webp" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Almacenamiento</h5>
+                            <p>Con su alta capacidad podrás instalar muchas aplicaciones sin temor a quedarte sin
+                                espacio.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./assets/img/004.webp" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Portabilidad</h5>
+                            <p>Lleválo a todos lados sin temor a quedarte sin bateria.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='contenedor-carousel-control'>
+                <button class="carousel-control-anterior" type="button" data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Anterior</span>
+                </button>
+                <button class="carousel-control-siguiente" type="button" data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Siguiente</span>
+                </button>
+            </div>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="./assets/img/001.webp" class="d-block w-100" alt="camara noche">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Cámara</h5>
-              <p>Saca las mejores fotos con su nueva cámara de alta definición, incluso de noche.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="./assets/img/002.webp" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Redes sociales</h5>
-              <p>Mantente conectado a todos lados desde un solo dispositivo.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="./assets/img/003.webp" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Almacenamiento</h5>
-              <p>Con su alta capacidad podrás instalar muchas aplicaciones sin temor a quedarte sin espacio.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="./assets/img/004.webp" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Portabilidad</h5>
-              <p>Lleválo a todos lados sin temor a quedarte sin bateria.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class='contenedor-carousel-control'>
-        <button class="carousel-control-anterior" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Anterior</span>
-        </button>
-        <button class="carousel-control-siguiente" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Siguiente</span>
-        </button>
-      </div>
     </div>
-  </div>
 </section>
 
 <!-- Fin de seccion Principal -->
